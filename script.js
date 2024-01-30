207ec616e892b72c7b980802efc87aba2051f746
joelgogo15-ZabuYz <joelgogo15@gmail.com>
2024-01-30 21:43:45 +0000

    added API but couldnt append correctly

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 var APIKey = "0b87f1e0b1962deefc64c45dfa0b603f";
 
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.12579&appid=" + APIKey;
-             
+var locationInputEl = $('#location')             
  //Make the API request using Fetch
-fetch(queryURL)
-  .then(function (response){
-    return response.json();
-  })
-  .then(function(data){
-
-    document.getElementById("search-form").addEventListener("submit", function (event) {
-        event.preventDefault();
+    var submitAPI =  function (event) {
+      event.preventDefault();
       
-        var searchInput = document.getElementById("search-input").value;
-        if (searchInput.trim() !== "") {
-          getWeatherData(searchInput)
-            .then(function (data) {
-              // Handle the data and update UI
-              console.log(data);
-              updateTodaySection(data);
-              updateForecastSection(data);
-            });
-        }
-      });
+      var locationInput = locationInputEl.val()
+      if (!locationInput) {
+        console.log('You need to fill out the form!');
+        return;
+      }
 
+      fetch(queryURL)
+      .then(function (response){
+        return response.json();
+      })
+      .then(function(data){
  
-  console.log(data)  
-  console.log(data.city.name)
+    console.log(data)
 
-  localStorage.setItem("city", data.city)
+  //localStorage.setItem('city',data.city.name)
 
-  })
+  }) 
+  .catch(function (error){
+    console.log("Error fetching data:" , error);
+  });
+    }
 
+
+    document.getElementById("search-form").addEventListener("submit", submitAPI);
+
+  /* I run into issues trying to get the API data to show on the screen 
+  I think I needed to append the data or something like that */
